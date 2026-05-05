76e8eac65845c0d0e3590fb5024e9029b35dba89
Shashika Learning <shashikalearning@gmail.com>
2026-05-05 10:29:46 +0530

    Add clsx package for conditional class names and refactor tab layout to use constants for tab data. Update screens to use SafeAreaView for better layout handling.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
- 
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -16,7 +19,7 @@ export default function App() {
         pathname: "/subscriptions/[id]",
         params: { id: "spotify" },
       }} className="mt-4 rounded bg-primary text-white p-4">Spotify Subscription</Link>
-    </View>
+    </SafeAreaView>
   );
   
 }
